c65a63d0f0f0452808a1c065c2f4838cbd96394e
Marcio <marcio.augusto.o.s@gmail.com>
2024-07-21 11:28:10 -0300

    inicio projeto

---
+++ src/main/java/com/manager/sectorsync/SectorsyncApplication.java
@@ -0,0 +1,13 @@
+package com.manager.sectorsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/sectorsync/SectorsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.sectorsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
